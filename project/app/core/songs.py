34e76f472d6228df97420d19f565f7dcd3839651
Pablo Monteiro <pablo.monteiro@2rpnet.com>
2021-12-28 17:09:21 -0300

    subindo migrations

---
+++ project/app/core/songs.py
@@ -0,0 +1,27 @@
+from fastapi import Depends, FastAPI, APIRouter
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db import get_session, init_db
+from app.models import Song, SongCreate
+
+router = APIRouter(
+    prefix='/songs',
+    tags=["songs"],
+    responses={404: {"description": "Not found"}},
+)
+
+@router.get("/songs", response_model=list[Song])
+async def get_songs(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Song))
+    songs = result.scalars().all()
+    return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
+
+
+@router.post("/songs")
+async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
+    song = Song(name=song.name, artist=song.artist, year=song.year)
+    session.add(song)
+    await session.commit()
+    await session.refresh(song)
+    return song
--- project/app/main.py
+++ project/app/main.py
@@ -1,8 +1,50 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, APIRouter
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette_prometheus import metrics, PrometheusMiddleware
 
-app = FastAPI()
+from app.db import get_session, init_db
+
+import os
+import time
+
+from starlette.requests import Request
+
+from .core import songs
+
+tags_metadata = [
+    {"name": "index", "description": "Welcome Zephyrus and best practices."},
+]
+
+_prefix_api = os.getenv('PREFIX_API', "/motor")
+
+api_version = "0.1.1"
+app = FastAPI(
+    title="songs", 
+    version=api_version,
+    description="songs", 
+    openapi_tags=tags_metadata,
+    docs_url=f"{_prefix_api}/", 
+
+    openapi_url=f"{_prefix_api}/openapi.json",
+
+)
+app.router.redirect_slashes = False
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    start_time = time.time()
+
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
-@app.get("/ping")
-async def pong():
-    return {"ping": "pong!"}
+app.include_router(songs.router, prefix=f"{_prefix_api}")
+
+app.add_middleware(PrometheusMiddleware)
+app.add_route(f"{_prefix_api}/metrics/", metrics)
+
+
